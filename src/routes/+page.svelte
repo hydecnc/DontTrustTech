315edf6fb368c141ed8308a26d5631ef17957b77
Hyde Yoo <seokjinyoo0716@gmail.com>
2024-10-26 20:09:53 -0400

    added phishing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,73 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import { goto } from "$app/navigation";
+	import { user } from "$lib/store";
+	import { fade } from "svelte/transition";
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
+	let username: string = "";
+	let invalidInput: boolean = false;
+	const registerUser = async () => {
+		console.log(username);
+		if (username == "") {
+			invalidInput = true;
+			return -1;
+		}
+		user.set(username);
+		await goto("phishing");
+	};
+</script>
+
+<h1 class="h1 text-center m-5 text-6xl">Don't Trust Tech.</h1>
+<div class="justify-center align-middle flex-col gap-2 text-center">
+	<div class="badge variant-ghost-warning my-4">Make sure to turn your volume up for the best experience.</div>
+	<div class="h2 text-4xl mb-6">Enter your username here to get started:</div>
+	<input
+		class="p-1 text-base max-w-52 text-black"
+		placeholder="your username here"
+		bind:value={username}
+	/>
 </div>
+<div class="start">
+	<button on:click={registerUser} class="btn variant-outline-primary"
+		>I'm Ready</button
+	>
+	{#if invalidInput}
+		<aside class="alert variant-ghost-warning" transition:fade={{ duration: 200 }}>
+			Please type in your username before proceeding!
+		</aside>
+	{/if}
+</div>
+
+<style>
+	#dtt {
+		font-size: 6em;
+		text-align: center;
+		margin-bottom: 8px;
+	}
+	.userinput {
+		font-size: 2em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		gap: 8px;
+	}
+	.userinput input {
+		padding: 4px;
+		font-size: medium;
+	}
+	.start {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
+	.start button {
+		font-size: 1.5rem;
+		font-weight: bold;
+		height: 3rem;
+		margin: 30px;
+	}
+	.warning {
+		color: red;
+	}
+</style>
---
+++ src/routes/phishing/+page.svelte
@@ -0,0 +1,127 @@
+<script lang="ts">
+    import Email from "$lib/Email.svelte";
+    import {
+        initializeStores,
+        Modal,
+        getModalStore,
+    } from "@skeletonlabs/skeleton";
+    import type {
+        ModalSettings,
+        ModalComponent,
+        ModalStore,
+    } from "@skeletonlabs/skeleton";
+    import kiss from "$lib/assets/gif/kiss.gif";
+
+    const modalCorrect: ModalSettings = {
+        type: "alert",
+        // Data
+        title: "Example Alert",
+        body: "This is an example modal.",
+        image: "https://i.imgur.com/WOgTG96.gif",
+    };
+    const modalWrong: ModalSettings = {
+        type: "alert",
+        // Data
+        title: "This is a possible phishing email.",
+        body: "",
+        image: "https://i.imgur.com/WOgTG96.gif",
+    };
+
+    initializeStores();
+    const modalStore = getModalStore();
+
+    let index = 0;
+
+    const phishingEmail = {
+        title: "Congratulations! You've Been Selected to Receive $200!",
+        content: `
+We are excited to inform you that you've been chosen for a special reward of $200 as part of our loyal customer appreciation program! To claim your $200, please follow the steps below. But hurry, as this limited offer expires in 24 hours!
+
+<strong>How to Claim Your Reward</strong>:
+
+    1. Click on the secure link below to access our reward center:  
+    Claim My $200 Now: https://fake-rewards.com/claim
+
+    2. Log in using your account details to verify your identity.
+
+    3. Complete a short verification process to confirm your eligibility.
+
+Once you've verified your account, your $200 will be credited directly to your account within minutes!
+
+Please note: Failure to claim your reward within 24 hours will result in forfeiture. Don't miss this opportunity!
+
+If you have any questions, please feel free to reply to this email, and our customer service team will be happy to assist you.
+
+`,
+        answer: true,
+        explanation:
+            "This email is a phishing attempt because it promotes a sense of urgency and offers a reward that seems too good to be true, encouraging you to click on a suspicious link and provide personal information. Legitimate companies typically do not ask for sensitive details or direct you to log in via email links.",
+        image: "https://i.imgur.com/TykCy5e.gif",
+    };
+
+    const realEmail = {
+        title: "Follow-Up on Our Recent Discussion",
+        content: `
+I hope this message finds you well. I wanted to follow up on our recent conversation regarding [specific topic or project]. I appreciate your insights and would love to explore this further.
+
+If you're available, I'd like to schedule a time to discuss our ideas in more detail. Please let me know your availability over the next few days, and I'll do my best to accommodate.
+
+Thank you, and I look forward to hearing from you soon!
+`,
+        answer: false,
+        explanation:
+            "This email appears legitimate because it maintains a professional tone, references a previous conversation, and expresses a genuine interest in further discussion. Additionally, it invites the recipient to suggest their availability rather than demanding immediate action, which is characteristic of authentic communication in a business context.",
+        image: kiss,
+    };
+
+    const emails = [phishingEmail, realEmail];
+    let email: {
+        title: string;
+        content: string;
+        answer: boolean;
+        explanation: string;
+        image: string;
+    };
+
+    const checkReponse = async () => {
+        const modal: ModalSettings = {
+            type: "alert",
+            title: email.title,
+            body: email.explanation,
+            image: email.image,
+        };
+        if (email.answer) {
+            modalStore.trigger(modal);
+        } else {
+            modalStore.trigger(modal);
+        }
+        index++;
+        console.log(index);
+        if (index >= emails.length) {
+            return;
+        }
+        updateEmail(index);
+    };
+    const updateEmail = (index: number) => {
+        email = emails[index];
+    };
+    updateEmail(0);
+</script>
+
+<Email title={email.title} message={email.content} />
+
+<Modal />
+<div class="flex flex-row justify-center">
+    <button
+        class="btn variant-outline-primary mr"
+        on:click={() => {
+            checkReponse();
+        }}>Check Email</button
+    >
+</div>
+
+<style lang="postcss">
+    .mr {
+        margin-right: 10px;
+    }
+</style>
